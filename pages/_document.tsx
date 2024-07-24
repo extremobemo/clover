c00cf29b4b3d2a28c2cb5f3a19425a11d9aa4ba7
extremobemo <extremobemo@gmail.com>
2024-07-24 13:31:13 -0600

    CLVR-17: More mobile optimization tests

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Other head elements */}
+          <meta name="theme-color" content="#168816" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
