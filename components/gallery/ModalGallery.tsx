295ce7607b1b3ac7990ecde4c4c1642e0de649cd
Tanner Skelton <tanner.skelton84@gmail.com>
2024-09-16 18:01:08 -0500

    more cleanup

--- components/gallery/ModalGallery.tsx
+++ components/gallery/ModalGallery.tsx
@@ -12,14 +12,6 @@ const Modal = ({ onClose, public_id }) => {
         onClose();
     };
 
-    // const { maxScroll, position } = useScrollPosition("scroll-container2");
-
-    // useEffect(() => {
-    //     console.log("maxScroll", maxScroll);
-    //     console.log("position", position);
-    //     // Do something when maxScroll changes
-    // }, [maxScroll, position]);
-
     const modalContent = (
         <div className="modal-overlay">
             {/* Wrap the whole Modal inside the newly created StyledModalWrapper
@@ -29,7 +21,7 @@ const Modal = ({ onClose, public_id }) => {
                     <a style={{position: "absolute", top: 10, right: 10, fontSize: 32, width: 32, height: 32}} href="#" onClick={handleCloseClick}>
                         x
                     </a>
-                    <div className="modal-body" id="scroll-container2" >
+                    <div className="modal-body" >
                         <HorizontalGallery public_id={public_id}  />
                     </div>
                 </div>
