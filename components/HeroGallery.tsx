20cf9d677ea1db5ee91ee5918cbe939e3bc754d9
John Erwin <jerwin@penlink.com>
2024-10-14 18:27:22 -0500

    stonecutter brrrrr

--- components/HeroGallery.tsx
+++ components/HeroGallery.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { AdvancedImage } from '@cloudinary/react'; // Adjust according to how you're using Cloudinary
 import styles from '../styles/HeroGallery.module.css'; // Import CSS module
 import { Photo } from '../types/types';
+import {SpringGrid, measureItems, layout} from 'react-stonecutter'
 
 interface HeroGalleryProps {
   columns: Photo[][],
@@ -13,36 +14,40 @@ const preventRightClick = (e : React.MouseEvent) => {
   e.preventDefault();
 }
 
+const Grid = measureItems(SpringGrid)
+
 const HeroGallery : React.FC<HeroGalleryProps> = ({ columns, handleModal }) => {
   return (
     <div className={styles.galleryContainer}>
-      {columns.map((column, columnIndex) => (
-        <div key={columnIndex} className={styles.column}>
-          {column.map((photo, index) => (
-            <motion.div
-              key={index}
-              className={styles.photoContainer}
-              whileHover={{ scale: 1.01 }}
-              transition={{ duration: 0.3, ease: 'easeInOut' }}>
+     <Grid
+  component="ul"
+  columns={2}
+  columnWidth={350}
+  gutterWidth={10}
+  gutterHeight={5}
+  layout={layout.pinterest}
+  {...({ children: columns[0].map((photo, index) => (
+    <li key={index}>
+      <motion.div
+        className={styles.photoContainer}
+        whileHover={{ scale: 1.01 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}>
 
-                <motion.div whileHover={{ scale: 1.08 }} /* zoom in effect */
-                  transition={{ duration: 0.4, ease: 'easeInOut' }}
-                  className={styles.imageWrapper}>
+        <motion.div whileHover={{ scale: 1.08 }} transition={{ duration: 0.4, ease: 'easeInOut' }}
+          className={styles.imageWrapper}>
 
-                  <AdvancedImage
-                    onClick={() => handleModal(true, photo.folder)}
-                    onContextMenu={preventRightClick}
-                    cldImg={photo.image}
-                    className={styles.advancedImage}
-                    style={{ width: '100%' }}
-                  />
-
-                </motion.div>
-
-            </motion.div>
-          ))}
-        </div>
-      ))}
+          <AdvancedImage
+            onClick={() => handleModal(true, photo.folder)}
+            onContextMenu={preventRightClick}
+            cldImg={photo.image}
+            className={styles.advancedImage}
+            style={{ width: '100%' }}
+          />
+        </motion.div>
+      </motion.div>
+    </li>
+  ))})}
+/>
     </div>
   );
 };
