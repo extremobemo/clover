e9726c54502943fedee9ba121fceb59749126d81
extremobemo <extremobemo@gmail.com>
2024-11-10 18:11:53 -0700

    fix flashing green bar, fix breakpoints in hero layout, adjust height data

--- data/heightData.tsx
+++ data/heightData.tsx
@@ -3,28 +3,28 @@ const photoHeightData = {
     allPhotosHeightData: [
         [
             // Group 1
-            [3, 3, 14, 11, 4],   // Column 1
-            [10, 11, 6, 1, 0],    // Column 2
+            [0, 1, 9, 11, 0],   // Column 1
+            [10, 11, 9, 1, 0],    // Column 2
           ],
           [
              // Group 2
-            [13, 3, 10, 19, 7],   // Column 1
-            [10, 3, 12, 3, 8],     // Column 2
+            [7, 0, 10, 9, 5],   // Column 1
+            [10, 6, 4, 0, 12],     // Column 2
           ],
           [
              // Group 3
-            [6, 11, 16, 12, 13],    // Column 1
-            [0, 10, 8, 9, 10],    // Column 2
+            [6, 15, 16, 12, 13],    // Column 1
+            [5, 6, 0, 9, 10],    // Column 2
           ],
           [
              // Group 4
             [8, 4, 0, 6, 0],    // Column 1
-            [11, 8, 8, 6, 12],     // Column 2
+            [11, 8, 0, 6, 0],     // Column 2
           ],
           [
              // Group 5
-            [9, 7, 8, 2, 1],     // Column 1
-            [3, 5, 9, 10, 6],    // Column 2
+            [9, 7, 0, 6, 6],     // Column 1
+            [3, 0, 9, 10, 6],    // Column 2
           ],
           [
              // Group 6
@@ -35,12 +35,12 @@ const photoHeightData = {
     filteredPhotosHeightData: [
         [
             // Group 1
-            [3, 3, 14, 11, 4],   // Column 1
+            [3, 0, 12, 11, 8],   // Column 1
             [2, 11, 6, 8, 7],    // Column 2
           ],
           [
              // Group 2
-            [0, 3, 10, 19, 7],   // Column 1
+            [10, 0, 0, 0, 0],   // Column 1
             [1, 7, 3, 12, 12],     // Column 2
           ],
           [
