fb4c3c775c1fdb6b3767714f36be63454f1fd201
jerwin21 <johnerwin73@gmail.com>
2025-01-08 14:26:19 -0600

    rewrite index to be email signup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,182 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useState, ChangeEvent, FormEvent } from "react";
+import styles from "../styles/emailSubmit.module.css";
 
-import PageTransition from "../components/common/PageTransition";
-import { AdvancedImage } from '@cloudinary/react';
-import { Cloudinary, CloudinaryImage } from '@cloudinary/url-gen';
-import { auto } from '@cloudinary/url-gen/actions/resize';
-import Modal from '../components/HorizontalGallery/ModalGallery';
-import Curtain from '../components/Curtain';
-import HeroGallery from '../components/HeroGallery/HeroGallery';
-import { HeroImageData } from '../types/types';
-import { useAppContext } from '../context/AppContext';
-import CloverFooterButton from '../components/common/CloverFooterButton'
-import styles from '../styles/Index.module.css'
-import cloverProductions from './cloverProductions.json'
-import heightData from '../data/heightData'
-import IndexFooterButton from '../components/common/IndexFooterButton';
+const EmailSubmit: React.FC = () => {
+  const [email, setEmail] = useState<string>(""); // State for email input
+  const [errorMessage, setErrorMessage] = useState<string>(""); // State for error message
+  const [successMessage, setSuccessMessage] = useState<string>(""); // State for success message
+  const [canSubmit, setCanSubmit] = useState<boolean>(false); // State for button enable/disable
+  const [submissionState, setSubmissionState] = useState<"PRESUBMIT" | "LOADING" | "SUCCESS" | "ERROR">("PRESUBMIT"); // Submission state
 
-interface Photo {
-  image: CloudinaryImage;
-  folder: string;
-}
+  // Basic email validation regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-const CloverProductionsSet  = new Set(
-[
-  ...cloverProductions  
-]);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
 
-const allPhotosHeightData = heightData.allPhotosHeightData;
-
-const filteredPhotosHeightData = heightData.filteredPhotosHeightData;
-
-
-const HeroPage: React.FC = () => {
-  const router = useRouter();
-  const { showModal, modalState, publicId, openModal, closeModal, heroFilterState } = useAppContext();
-
-  const [allPhotos, setAllPhotos] = useState<Photo[]>([]);
-  const [productionPhotos, setProductionPhotos] = useState<Photo[]>([]);
-  const [visiblePhotos, setVisiblePhotos] = useState<Photo[]>([]);
-
-  const [heightData, setHeightData] = useState<number[][][]>(allPhotosHeightData);
-
-  const [isFiltered, setIsFiltered] = useState<Boolean>(false);
-
-  const [imageOffScreen, setImageOffScreen] = useState<boolean>(false);
-  const [showGreenBar, setShowGreenBar] = useState<boolean>(false);
-
-  const cld = new Cloudinary({ cloud: { cloudName: 'ddlip2prr' } });
-
-
-  //when router is ready, grab arguments from router to determine which modal to display. This helps us with browser navigation stuff. 
-  useEffect(() => {
-    //TODO: will need to scrub the url param to handle invalid args
-
-    if (!router.isReady) return; // Wait until the router is ready
-
-    const gallery = router.query.gallery;
-    const page = router.query.page;
-    if (typeof gallery === 'string') {
-      openModal('gallery', gallery)
-
-    } else if(typeof page ==='string'){
-      openModal(page, null);
+    if (emailRegex.test(newEmail)) {
+      setCanSubmit(true);
     } else {
-      closeModal();
+      setCanSubmit(false);
     }
-  }, [router.query.gallery, router.query.page, router.isReady]);
+  };
 
-  // Fetch Hero Photos
-  useEffect(() => {
-    const fetchPhotos = async () => {
-      try {
-        const response = await fetch('/api/photos');
-        const data = await response.json();
-  
-        const cloudinaryPhotos : Photo[] = data.map((photo: HeroImageData) => {
-          const cloudImage : CloudinaryImage = cld.image(photo.public_id).resize(auto().width(1000));
-          return {
-            image: cloudImage,
-            folder: photo.folder,
-          };
-        });
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        setAllPhotos(cloudinaryPhotos);
-       
-        let productionPhotos = cloudinaryPhotos.filter(photo => CloverProductionsSet.has(photo.folder));
-        setProductionPhotos(productionPhotos);
+    if (!emailRegex.test(email)) {
+      setErrorMessage("Please enter a valid email address.");
+      setSuccessMessage(""); // Clear success message if any
+      setSubmissionState("ERROR");
+      return;
+    }
 
-        //TODO: once we have videos, will need to create a set of folder names kinda like we've got for CloverProductions 
-        // and use that to create the video set
+    setSubmissionState("LOADING"); // Set loading state
 
+    try {
+      const response = await fetch("/api/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data: email }), // Send email to the API
+      });
 
-        //initialize photos to display as all photos
-        setVisiblePhotos(cloudinaryPhotos);
+      const result = await response.json();
 
-        setHeightData(allPhotosHeightData);
-      } catch (error) {
-        console.error('Error fetching photos:', error);
+      if (response.ok) {
+        setSuccessMessage(result.message);
+        setErrorMessage(""); // Clear error message if any
+        setSubmissionState("SUCCESS");
+      } else {
+        setErrorMessage(result.message || "Something went wrong.");
+        setSuccessMessage(""); // Clear success message if any
+        setSubmissionState("ERROR");
       }
-    };
-
-    fetchPhotos();
-  }, []);
-
-  //might need a use effect for the heroFilterState
-  useEffect(() => {
-    console.log(`heroFilterState updated: ${heroFilterState}`)
-
-    switch (heroFilterState) {
-      case 'ALL':
-        setVisiblePhotos(allPhotos);
-        setHeightData(allPhotosHeightData);
-        break;
-      case 'CLOVERPRODUCTION':
-        setVisiblePhotos(productionPhotos);
-        setHeightData(filteredPhotosHeightData);
-        break;
-      case 'VIDEO': //TODO: once we've created new set, utilize that.
-        setVisiblePhotos(productionPhotos);
-        setHeightData(filteredPhotosHeightData);
-        break;
-      default:
-        setVisiblePhotos(allPhotos);
-        setHeightData(allPhotosHeightData);
-        break;
+    } catch (error) {
+      console.error("Error submitting email:", error);
+      setErrorMessage("Failed to submit email. Please try again.");
+      setSuccessMessage(""); // Clear success message if any
+      setSubmissionState("ERROR");
     }
-    
-  }, [heroFilterState])
-
-
-  const toggleFilter = () =>
-  {
-    if(isFiltered){
-      setVisiblePhotos(allPhotos)
-      setHeightData(allPhotosHeightData)
-    }
-    else{
-      setVisiblePhotos(productionPhotos)
-      setHeightData(filteredPhotosHeightData)
-    }
-
-    setIsFiltered(!isFiltered);
-  }
-  
+  };
 
   return (
-    <div id="outermost_div"
-      style={{ display: 'flex', justifyContent: 'center',
-      overflowY: 'hidden', height: '100dvh', overflowX: 'hidden'}} >
+    <div className={styles.container}>
 
-      {!imageOffScreen && (
-        <Curtain imageOffScreen={imageOffScreen} setImageOffScreen={setImageOffScreen} setShowGreenBar={setShowGreenBar}/>
+      {submissionState === "PRESUBMIT" && (
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <h1 className={styles.tapIn}>Welcome to Clover</h1>
+          <input
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            onChange={handleChange}
+            className={styles.input}
+          />
+          <button type="submit" className={styles.button} disabled={!canSubmit}>
+            Submit
+          </button>
+          {errorMessage && <p className={styles.error}>{errorMessage}</p>}
+        </form>
       )}
-
-      {/* {showGreenBar && (<GreenBar text="CLOVER." />)} */}
-      
-      <div id="content_div" style={{
-        display: 'flex',
-        position: 'absolute',
-        zIndex: 1,
-        justifyContent: 'center',
-        overflowY: 'hidden',
-        overflowX: 'hidden',
-        width: '100vw',
-        maxWidth: '100vw',
-      }}>
-
-        <HeroGallery photos={visiblePhotos} heightData={heightData} /> 
-
-      </div>
-
-      {showModal && <Modal state={modalState} onClose={ () => closeModal()} public_id={publicId} /> }
-      <CloverFooterButton/>
-      <IndexFooterButton/>
-      {/* <button className={styles.invisibleFilterToggleButton} onClick={toggleFilter} disabled={showModal}/> */}
+      {submissionState === "LOADING" && (
+        <div className={styles.loadingOverlay}>
+          <div className={styles.spinner}></div>
+        </div>
+      )}
+      {submissionState === "SUCCESS" && <p className={styles.success}>{successMessage}</p>}
+      {submissionState === "ERROR" && <h1 className={styles.submissionError}>{errorMessage}</h1>}
     </div>
-    
   );
 };
 
-export default HeroPage;
+export default EmailSubmit;
