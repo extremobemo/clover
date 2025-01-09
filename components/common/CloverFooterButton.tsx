ef18733eed486f67fe33d4d26974e03846ddd228
extremobemo <extremobemo@gmail.com>
2025-01-08 22:37:44 -0700

    Invert both menus

--- components/common/CloverFooterButton.tsx
+++ components/common/CloverFooterButton.tsx
@@ -4,40 +4,37 @@ import styles from '../../styles/CloverFooter.module.css';
 import { useAppContext } from '../../context/AppContext';
 
 const menuVariants: Variants = {
-    hidden: {
-      opacity: 0,
-      scale: 0.8,
-      y: 20, // Start slightly below
-      x: 10,
+  hidden: {
+    opacity: 0,
+    scale: 0.8,
+    y: 20, // Start slightly below
+    x: 10,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    y: 0, // Move to its final position
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+      damping: 25,
     },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      y: 0, // Move to its final position
-      x: 0,
-      transition: {
-        type: "spring",
-        stiffness: 300,
-        damping: 25,
-      },
-    },
-    exit: {
-      opacity: 0,
-      scale: 0.8,
-      y: 20,
-      x:10,
-      transition: { duration: 0.2 },
-    },
-  };
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.8,
+    y: 20,
+    x: 10,
+    transition: { duration: 0.2 },
+  },
+};
 
 
 export default function CloverFooterButton() {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const { openModal } = useAppContext();
   const menuRef = useRef<HTMLDivElement>(null); // Typed ref for the menu container
-
-  
-
   const toggleMenu = () => setMenuOpen(!isMenuOpen);
 
   // Close menu if clicked outside
@@ -61,28 +58,28 @@ export default function CloverFooterButton() {
 
   return (
     <div className={styles.footerContainer} ref={menuRef}>
-        <AnimatePresence>
+      <AnimatePresence>
         {isMenuOpen && (
-        <motion.div
-        className={styles.menu}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          variants={menuVariants}>
+          <motion.div
+            className={styles.menu}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            variants={menuVariants}>
             <div className={styles.menu}>
-                <motion.div 
-                    whileHover={{ scale: 1.1, x: -3 }}
-                 className={styles.menuItem} onClick={() => {window.open("https://shop.cianmoore.com", "_blank")?.focus(); setMenuOpen(false)}}>SHOP</motion.div>
-                <motion.div  whileHover={{ scale: 1.1, x: -3 }} className={styles.menuItem} onClick={() => {openModal('coffee', null); setMenuOpen(false)}}>COFFEE</motion.div>
-                <motion.div whileHover={{ scale: 1.1, x: -3 }} className={styles.menuItem} onClick={() => {openModal('about', null); setMenuOpen(false)}}>INFO/CONTACT</motion.div>
+              <motion.div whileHover={{ scale: 1.1, x: -3 }} className={styles.menuItem} onClick={() => { openModal('about', null); setMenuOpen(false) }}>INFO/CONTACT</motion.div>
+              <motion.div whileHover={{ scale: 1.1, x: -3 }} className={styles.menuItem} onClick={() => { openModal('coffee', null); setMenuOpen(false) }}>COFFEE</motion.div>
+              <motion.div
+                whileHover={{ scale: 1.1, x: -3 }}
+                className={styles.menuItem} onClick={() => { window.open("https://shop.cianmoore.com", "_blank")?.focus(); setMenuOpen(false) }}>SHOP</motion.div>
             </div>
-        </motion.div>
-            
-      )}
-        </AnimatePresence>
-      
+          </motion.div>
+
+        )}
+      </AnimatePresence>
+
       <button className={styles.footerButton} onClick={toggleMenu}>
-      {/* keep this bih empty */}
+        {/* keep this bih empty */}
       </button>
     </div>
   );
